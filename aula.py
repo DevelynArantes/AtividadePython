e2ea2b05d8cf4f1b5d89f52ab8c33a36a706c5d5
DevelynArantes <103970082+DevelynArantes@users.noreply.github.com>
2025-06-16 13:03:39 -0300

    Subindo o projeto simples de Python

---
+++ aula.py
@@ -0,0 +1,13 @@
+numerosecreto = 5
+
+tentativa = int(input("Tenta adivinhar um numero: "))
+
+while tentativa != numerosecreto:
+
+    print("Você errou. Tente novamente.")
+
+    tentativa = int(input("Digite novamente: "))
+
+
+
+print("Você acertou!")
